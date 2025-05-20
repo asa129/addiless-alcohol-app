14d644c376f90d54c02109464e3c17f044890f01
asa129 <asa.jad1290@gmail.com>
2025-05-20 21:46:40 +0900

    不要なimportを削除

--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,9 +1,9 @@
 import App from "../App";
-import {findByText, render, screen} from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 
 describe("title", () => {
-  it("should render title", async() => {
+  it("should render title", async () => {
     render(<App />);
     expect(await screen.findByText("Vite + React")).toBeInTheDocument();
-  })
+  });
 });
