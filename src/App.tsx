4f63f2a2f1d46f07333d81c54d072aa36c1d4c31
asa129 <asa.jad1290@gmail.com>
2025-05-25 11:28:25 +0900

    テスト追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,22 @@ import type { Alcohols } from "./domain/Alcohols.ts";
 
 function App() {
   const [data, setData] = useState<Partial<Alcohols>[]>();
+  const [isLoading, setIsLoading] = useState(true);
   const getData = async () => {
     const data = await getAllData();
     setData(data);
+    setIsLoading(false);
   };
   useEffect(() => {
     getData();
   }, []);
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
   return (
     <>
-      <h1>成分表示でお酒を検索</h1>
+      <h1 data-testid="title">成分表示でお酒を検索</h1>
       <table>
         <thead></thead>
         <tbody>
@@ -27,11 +32,17 @@ function App() {
             data.map((data) => {
               return (
                 <tr key={data.id}>
-                  <td>{data.sake_name}</td>
+                  <td data-testid="sake_name">{data.sake_name}</td>
                   <td>
-                    <img src={data.image_url!} alt={data.sake_name} />
+                    <img
+                      src={data.image_url!}
+                      alt={data.sake_name}
+                      data-testid="sake_image"
+                    />
                   </td>
-                  <td>{data.has_additives ? "あり" : "なし"}</td>
+                  <td data-testid="sake_additives">
+                    {data.has_additives ? "あり" : "なし"}
+                  </td>
                 </tr>
               );
             })}
--- src/tests/AppComponent.spec.tsx
+++ src/tests/AppComponent.spec.tsx
@@ -1,9 +1,66 @@
 import App from "../App";
 import { render, screen } from "@testing-library/react";
+import { getAllData } from "../utils/supabaseFunctions";
 
-describe("title", () => {
-  it("should render title", async () => {
+jest.mock("../utils/supabaseFunctions.ts", () => ({
+  getAllData: jest.fn(),
+}));
+
+describe("App", () => {
+  it("ページタイトルが表示されている", async () => {
     render(<App />);
-    expect(true).toBeTruthy();
+    expect(await screen.findByTestId("title")).toBeInTheDocument();
+  });
+
+  it("ローディングが表示されている", async () => {
+    render(<App />);
+    expect(await screen.findByText("Loading...")).toBeInTheDocument();
+  });
+
+  it("お酒のリストが正しく表示されている", async () => {
+    // モックデータの準備
+    const mockData = [
+      {
+        id: 1,
+        sake_name: "ビール",
+        image_url: "https://example.com/beer.jpg",
+        has_additives: true,
+      },
+      {
+        id: 2,
+        sake_name: "ワイン",
+        image_url: "https://example.com/wine.jpg",
+        has_additives: false,
+      },
+      {
+        id: 3,
+        sake_name: "サケ",
+        image_url: "https://example.com/sake.jpg",
+        has_additives: true,
+      },
+    ];
+
+    // モック関数の設定
+    (getAllData as jest.Mock).mockResolvedValue(mockData);
+
+    render(<App />);
+    expect(await screen.findAllByTestId("sake_name")).toHaveLength(3);
+    expect(await screen.findAllByTestId("sake_image")).toHaveLength(3);
+    expect(await screen.findAllByTestId("sake_additives")).toHaveLength(3);
+
+    const nameLows = await screen.findAllByTestId("sake_name");
+    expect(nameLows[0]).toHaveTextContent("ビール");
+    expect(nameLows[1]).toHaveTextContent("ワイン");
+    expect(nameLows[2]).toHaveTextContent("サケ");
+
+    const imageLows = await screen.findAllByTestId("sake_image");
+    expect(imageLows[0]).toHaveAttribute("src", "https://example.com/beer.jpg");
+    expect(imageLows[1]).toHaveAttribute("src", "https://example.com/wine.jpg");
+    expect(imageLows[2]).toHaveAttribute("src", "https://example.com/sake.jpg");
+
+    const additivesLows = await screen.findAllByTestId("sake_additives");
+    expect(additivesLows[0]).toHaveTextContent("あり");
+    expect(additivesLows[1]).toHaveTextContent("なし");
+    expect(additivesLows[2]).toHaveTextContent("あり");
   });
 });
