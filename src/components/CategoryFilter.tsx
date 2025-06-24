eaae24f8452be26aea1b0a1f2173721f7db3e7a7
asa129 <asa.jad1290@gmail.com>
2025-06-24 22:30:01 +0900

    カテゴリー追加

--- src/components/CategoryFilter.tsx
+++ src/components/CategoryFilter.tsx
@@ -47,24 +47,27 @@ export const CategoryFilter = (props: {
               カクテル
             </label>
           </div>
+          <div
+            className={`flex gap-2 items-center px-4 py-2 rounded-full text-sm font-medium transition-all duration-200 cursor-pointer min-w-fit ${
+              genreId.includes("1")
+                ? "bg-brand-blue text-white shadow-md"
+                : "bg-white hover:bg-brand-blue hover:text-white border"
+            }`}
+          >
+            <input
+              type="checkbox"
+              value="1"
+              id="beer"
+              onChange={handleChange}
+              checked={genreId.includes("1")}
+              data-testid="beer"
+              className="w-4 h-4 rounded border-2 border-current accent-current"
+            />
+            <label htmlFor="beer" className="cursor-pointer">
+              ビール
+            </label>
+          </div>
           {/* <div
-                  className={`flex gap-2 items-center px-4 py-2 rounded-full text-sm font-medium transition-all duration-200 ${
-                    false
-                      ? "bg-blue-500 text-white shadow-md hover:bg-blue-600"
-                      : "bg-gray-100 text-gray-700 hover:bg-gray-200 border border-gray-300"
-                  }`}
-                >
-                  <input
-                    type="checkbox"
-                    value="4"
-                    id="cocktail"
-                    onChange={handleChange}
-                    data-testid="cocktail"
-                    className="data-[state=checked]:bg-brand-blue data-[state=checked]:text-white border border-brand-blue w-5 h-5 rounded-md"
-                  />
-                  <label htmlFor="cocktail">カクテル</label>
-                </div>
-                <div
                   className={`flex gap-2 items-center px-4 py-2 rounded-full text-sm font-medium transition-all duration-200 ${
                     false
                       ? "bg-blue-500 text-white shadow-md hover:bg-blue-600"
